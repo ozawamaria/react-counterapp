cf8aa0872f2bd62a1e2591fca1761f0eea52b748
ozawamaria <2ty2maria1999@gmail.com>
2021-03-08 23:22:38 +0900

    add time

--- src/App.js
+++ src/App.js
@@ -6,8 +6,19 @@ const App = () => {
     return(
         <div className='ui container' style={{marginTop: '20px'}}>
             <Counter />
+            <Clock />
         </div>
     );
+}
+
+class Clock extends React.Component{
+      render(){
+        this.now=new Date();
+        this.time =`${this.now.getHours()}:${this.now.getMinutes()}:${this.now.getSeconds()}`
+       return <p>{this.time}</p>
+ };
 };
+
+setInterval('render()',1000);
 
 export default App;
